edf252801966266d8157db9f0b71d15f42c4c880
moonwu1229 <121375633+moonwu1229@users.noreply.github.com>
2023-08-01 18:37:43 +0800

    Util.scala

--- src/main/scala/Utils/Util.scala
+++ src/main/scala/Utils/Util.scala
@@ -26,18 +26,18 @@ object Util {
   }
 
   def edgesDistinct(edges: RDD[(Long, Long)]): RDD[(Long, Long)] = {
-    //边的2个顶点，转换成小的顶点在前，大的顶点在后
+    //Convert each edge to smaller vertex on the left and the larger vertex on the right.
     val newEdges = edges.map {
       case (e1, e2) =>
         if (e1 > e2) (e2, e1)
         else (e1, e2)
     }
-    //去重并返回
+    //Remove duplicate edges.
     newEdges.distinct()
   }
 
   def calTriangle(graph: Graph[(Int, Array[Long]), None.type], graphType: Int): RDD[(Long, (Long, Long))] = {
-    //通过每条边计算Vα每个顶点的三角形数量
+    //Count the number of triangles of per vertex in Vs.
     val counters: VertexRDD[(Long, Long)] = graph.aggregateMessages[(Long, Long)](ctx => {
       val n1 = ctx.srcAttr._2
       val n2 = ctx.dstAttr._2
@@ -55,7 +55,7 @@ object Util {
           i += 1
         }
       }
-      if (ctx.srcAttr._1 == 1) ctx.sendToSrc(counter, 1)  //(三角数量， 度)
+      if (ctx.srcAttr._1 == 1) ctx.sendToSrc(counter, 1)  //(Number of triangles, degree)
       if (ctx.dstAttr._1 == 1) ctx.sendToDst(counter, 1)
     }, (attr1, attr2) => (attr1._1 + attr2._1, attr1._2 + attr2._2))
     counters
@@ -64,22 +64,20 @@ object Util {
 
   def findVaddData(ss: SparkSession, VB: RDD[Long], filePath: String, VPath: String): RDD[String] = {
     import ss.implicits._
-    //从原图找出VB的邻接链
-    //1.读取所有顶点来自哪个文件
+    //Find vertices in V_{neighbors} from NRPs not loaded.
+    //1.Read the data block contained vertices in V_{neighbors}.
     val blockNo: Column = substring_index(input_file_name(), "/", -1)
     val VNRPs: DataFrame = ss.read.text(VPath).withColumn("blockNo", blockNo)
-    //2.VB join 所有顶点后，收集文件名
     val frame: DataFrame = VB.toDF("id").join(VNRPs, col("id").cast("long") === col("value").cast("long"))
     val blockNames: Array[String] = frame.select("blockNo").distinct().rdd.map(filePath + "/" + _.getString(0)).collect()
-    //3.读取对应的块，找出VB的邻接链表
+    //2.From the read NRP, find the linked lists of vertices in V_{neighbors}.
     val block: RDD[String] = ss.sparkContext.textFile(blockNames.mkString(","))
     block
   }
 
   def sampleNRPs(sc: SparkContext, filePath: String, samplingRatio: Double): RDD[String] = {
-    //读取文件夹里的文件列表
+    //Randomly select NRP.
     val NRPs: List[String] = Util.getFiles(sc, filePath)
-    //抽样块
     val NRPSample: List[String] = Util.sample(NRPs, samplingRatio)
     val Gs: RDD[String] = sc.textFile(NRPSample.mkString(","))
     Gs
